7ec52e93c779710bc3fd152f2d11ea985929cfe8
Kamil Cholewiński <harry666t@gmail.com>
2018-05-10 13:20:04 +0200

    Refactor into editor

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,37 @@ use termios::*;
 struct Editor {
     cx: i16,
     cy: i16,
+
+    fd: RawFd,
+    orig_termios: Option<Termios>,
 }
 
 impl Editor {
-    fn new() -> Editor {
-        Editor{
+    fn new(fd: RawFd) -> Editor {
+        Editor {
             cx: 1,
             cy: 1,
+            fd: fd,
+            orig_termios: Option::None,
         }
     }
-}
 
-fn enable_raw_mode(fd: RawFd) -> io::Result<()> {
-    let mut termios = Termios::from_fd(fd)?;
-    tcgetattr(fd, &mut termios)?;
-    termios.c_lflag &= !(ECHO);
-    tcsetattr(fd, TCSAFLUSH, &termios)?;
-    Ok(())
-}
+    fn enable_raw_mode(&mut self) -> io::Result<()> {
+        let mut termios = Termios::from_fd(self.fd)?;
+        termios.c_lflag &= !ECHO;
+        tcsetattr(self.fd, TCSAFLUSH, &termios)?;
+        tcgetattr(self.fd, &mut termios)?;
+        self.orig_termios = Some(termios);
+        Ok(())
+    }
 
-fn disable_raw_mode(fd: RawFd) -> io::Result<()> {
-    let mut termios = Termios::from_fd(fd)?;
-    tcgetattr(fd, &mut termios)?;
-    termios.c_lflag |= (ECHO);
-    tcsetattr(fd, TCSAFLUSH, &termios)?;
-    Ok(())
+    fn disable_raw_mode(&self) -> io::Result<()> {
+        match self.orig_termios {
+            Some(termios) => tcsetattr(self.fd, TCSAFLUSH, &termios)?,
+            None => (),
+        }
+        Ok(())
+    }
 }
 
 fn read_char(fd: &mut io::Read) -> io::Result<u8> {
@@ -45,21 +51,22 @@ fn read_char(fd: &mut io::Read) -> io::Result<u8> {
 }
 
 fn main() {
-    let e = Editor::new();
     let args: Vec<String> = env::args().collect();
     let mut stdin = io::stdin();
-    let fd = stdin.as_raw_fd();
+    let mut e = Editor::new(stdin.as_raw_fd());
 
     println!("args: {:?}", args);
+    println!("e: {:?}", e);
 
-    enable_raw_mode(fd).expect("could not enable raw mode");
+    e.enable_raw_mode().expect("could not enable raw mode");
     loop {
         let ch = read_char(&mut stdin).expect("could not read char");
         println!("ch: {:?}", ch);
         if ch == 'q' as u8 {
-            break
+            break;
         }
     }
 
-    disable_raw_mode(fd).expect("could not disable raw mode");
+    e.disable_raw_mode().expect("could not disable raw mode");
+    println!("e: {:?}", e);
 }
