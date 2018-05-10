780b923ff0b4c148257946c719b1062e59c57e13
Kamil Cholewiński <harry666t@gmail.com>
2018-05-10 16:49:00 +0200

    Get window size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate libc;
 extern crate termios;
 
 use std::env;
@@ -8,8 +9,8 @@ use termios::*;
 
 #[derive(Debug)]
 struct Editor {
-    cx: i16,
-    cy: i16,
+    cx: u16,
+    cy: u16,
 
     term: Terminal,
 }
@@ -31,6 +32,21 @@ impl Editor {
             /* other */ _ => Command::KeyPress { ch: ch },
         })
     }
+
+    fn refresh_screen(&mut self) -> io::Result<()> {
+        self.term.stdout.write(b"\x1b[2J")?;
+        self.term.stdout.write(b"\x1b[H")?;
+        self.draw_rows()?;
+        self.term.stdout.write(b"\x1b[H")?;
+        Ok(())
+    }
+
+    fn draw_rows(&mut self) -> io::Result<()> {
+        for _y in 1..self.term.wy {
+            self.term.stdout.write(b"~\r\n")?;
+        }
+        Ok(())
+    }
 }
 
 enum Command {
@@ -44,6 +60,8 @@ struct Terminal {
     stdin: Box<io::Stdin>,
     stdout: Box<io::Stdout>,
     orig_termios: Option<Termios>,
+    wx: u16,
+    wy: u16,
 }
 
 impl Terminal {
@@ -52,6 +70,8 @@ impl Terminal {
             stdin: Box::new(stdin),
             stdout: Box::new(stdout),
             orig_termios: Option::None,
+            wx: 0,
+            wy: 0,
         }
     }
 
@@ -83,10 +103,22 @@ impl Terminal {
         read_char(&mut self.stdin)
     }
 
-    fn refresh_screen(&mut self) -> io::Result<()> {
-        self.stdout.write(b"\x1b[2J")?;
-        self.stdout.write(b"\x1b[H")?;
-        Ok(())
+    fn update_window_size(&mut self) -> io::Result<()> {
+        let fd = self.stdin.as_raw_fd();
+        let mut ws = libc::winsize {
+            ws_col: 0,
+            ws_row: 0,
+            ws_xpixel: 0,
+            ws_ypixel: 0,
+        };
+        match unsafe { libc::ioctl(fd, libc::TIOCGWINSZ, &mut ws) } {
+            -1 => Err(io::Error::last_os_error()),
+            _ => {
+                self.wx = ws.ws_col;
+                self.wy = ws.ws_row;
+                Ok(())
+            }
+        }
     }
 }
 
@@ -97,24 +129,24 @@ fn read_char(reader: &mut io::Read) -> io::Result<u8> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let _args: Vec<String> = env::args().collect();
     let t = Terminal::new(io::stdin(), io::stdout());
     let mut e = Editor::new(t);
 
-    println!("args: {:?}", args);
-    println!("e: {:?}", e);
-
     e.term.enable_raw_mode().expect("could not enable raw mode");
+    e.refresh_screen().expect("refresh_screen");
     loop {
-        e.term.refresh_screen().expect("refresh_screen");
+        e.term.update_window_size().expect("update_window_size");
         let cmd = e.process_input().expect("process_input");
         match cmd {
             Command::Nothing => (),
             Command::Exit => {
-                e.term.refresh_screen().expect("refresh_screen");
+                e.refresh_screen().expect("refresh_screen");
                 break;
             }
-            Command::KeyPress { ch } => print!("ch: {:?}\r\n", ch),
+            Command::KeyPress { ch: _ } => {
+                e.refresh_screen().expect("refresh_screen");
+            }
         }
     }
 
