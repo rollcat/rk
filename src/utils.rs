31a548af6354a92495c2a1183cd1c6f33ebc38e7
Kamil Cholewinski <harry666t@gmail.com>
2020-01-13 14:29:09 +0100

    string utils

---
+++ src/utils.rs
@@ -0,0 +1,43 @@
+pub trait StringUtils {
+    fn uslice(&self, start: usize, end: usize) -> String;
+}
+
+impl StringUtils for str {
+    fn uslice(&self, start: usize, end: usize) -> String {
+        self.chars().take(end).skip(start).collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_uslice_basic() {
+        assert_eq!("".uslice(0, 0), "");
+        assert_eq!("".uslice(1, 2), "");
+
+        assert_eq!("a".uslice(0, 0), "");
+        assert_eq!("a".uslice(0, 1), "a");
+        assert_eq!("a".uslice(0, 2), "a");
+        assert_eq!("a".uslice(1, 2), "");
+
+        assert_eq!("ab".uslice(0, 1), "a");
+        assert_eq!("ab".uslice(0, 2), "ab");
+        assert_eq!("ab".uslice(0, 3), "ab");
+
+        assert_eq!("ab".uslice(1, 1), "");
+        assert_eq!("ab".uslice(2, 3), "");
+        assert_eq!("ab".uslice(1, 2), "b");
+        assert_eq!("ab".uslice(1, 3), "b");
+    }
+
+    #[test]
+    fn test_uslice_unicode() {
+        assert_eq!("καλημέρα".uslice(0, 0), "");
+        assert_eq!("καλημέρα".uslice(0, 4), "καλη");
+        assert_eq!("καλημέρα".uslice(4, 8), "μέρα");
+        assert_eq!("καλημέρα".uslice(4, 6), "μέ");
+        assert_eq!("καλημέρα".uslice(4, 8), "μέρα");
+    }
+}
