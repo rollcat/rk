f4e59e45c96992966d8ac52166d47ec0e465dd09
Kamil Cholewinski <harry666t@gmail.com>
2020-01-14 13:22:32 +0100

    Make editor internals private

--- src/editor.rs
+++ src/editor.rs
@@ -2,23 +2,26 @@ use std::fs::File;
 use std::io::{self, BufRead, Write};
 use std::path::Path;
 
+use keys::Direction::*;
 use keys::*;
 use tty::Terminal;
 use utils::*;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
+pub struct Exit;
+
 #[derive(Debug)]
 pub struct Editor {
     // Cursor position (in file)
-    pub cx: u32,
-    pub cy: u32,
+    cx: u32,
+    cy: u32,
     // Offset (window scrolling)
-    pub ox: u32,
-    pub oy: u32,
+    ox: u32,
+    oy: u32,
 
-    pub term: Terminal,
-    pub lines: Vec<String>,
+    term: Terminal,
+    lines: Vec<String>,
 }
 
 impl Editor {
@@ -31,6 +34,117 @@ impl Editor {
             term: term,
             lines: Vec::new(),
         }
+    }
+
+    pub fn init(&mut self) -> io::Result<()> {
+        self.term.enable_raw_mode()?;
+        self.term.update_window_size()?;
+        self.term.clear_screen()?;
+        self.refresh_screen()?;
+        Ok(())
+    }
+
+    pub fn update(&mut self) -> io::Result<Option<Exit>> {
+        self.term.update_window_size()?;
+
+        let km = self.term.get_key()?;
+        eprintln!("key: {:?}", km);
+        let KeyMod {
+            key,
+            ctrl,
+            meta,
+            shift,
+        } = km;
+        let cmd = match (ctrl, meta, shift, key) {
+            (true, false, false, Key::Char('q')) => Command::Exit,
+            (false, false, _, Key::Direction(d)) => Command::Move(d),
+            (true, false, _, Key::Direction(d)) => match d {
+                Direction::Up => Command::MovePageUp,
+                Direction::Down => Command::MovePageDown,
+                Direction::Left => Command::MoveLineHome,
+                Direction::Right => Command::MoveLineEnd,
+            },
+            (false, false, _, Key::Char(ch)) => Command::InsertCharacter(ch),
+            (false, false, _, Key::PageUp) => Command::MovePageUp,
+            (false, false, _, Key::PageDown) => Command::MovePageDown,
+            (false, false, _, Key::Home) => Command::MoveLineHome,
+            (false, false, _, Key::End) => Command::MoveLineEnd,
+            (false, false, _, Key::Backspace) => Command::Erase(Left),
+            (false, false, _, Key::Delete) => Command::Erase(Right),
+            (_, _, _, key) => {
+                eprintln!(
+                    "unhandled: {:?}",
+                    KeyMod {
+                        key,
+                        ctrl,
+                        meta,
+                        shift,
+                    }
+                );
+                Command::Nothing
+            }
+        };
+
+        match cmd {
+            Command::Nothing => (),
+            Command::Exit => {
+                self.refresh_screen()?;
+                return Ok(Some(Exit));
+            }
+            Command::InsertCharacter(ch) => {
+                eprintln!("typing: {}", ch);
+            }
+            Command::Move(d) => match d {
+                Direction::Left => {
+                    if self.cx > 0 {
+                        self.cx -= 1
+                    }
+                }
+                Direction::Right => self.cx += 1,
+                Direction::Up => {
+                    if self.cy > 0 {
+                        self.cy -= 1
+                    }
+                }
+                Direction::Down => self.cy += 1,
+            },
+            Command::MovePageUp => {
+                self.cx = 0;
+                self.cy = 0;
+            }
+            Command::MovePageDown => {
+                self.cx = 0;
+                self.cy = self.term.wy - 1;
+            }
+            Command::MoveLineHome => {
+                self.cx = 0;
+            }
+            Command::MoveLineEnd => {
+                self.cx = self.term.wx - 1;
+            }
+            Command::Erase(Left) => {
+                if self.cx > 0 {
+                    self.cx -= 1
+                }
+            }
+            Command::Erase(Right) => self.cx += 1,
+            Command::Erase(_) => (),
+        }
+        // Scroll to show cursor
+        self.ox = 0;
+        while (self.cx - self.ox) >= (self.term.wx as f32 * 0.90) as u32 {
+            self.ox += (self.term.wx as f32 * 0.85) as u32;
+        }
+        self.refresh_screen().expect("refresh_screen");
+        eprintln!(
+            "editor: {{ cx: {}, cy: {}, ox: {}, oy: {} }}",
+            self.cx, self.cy, self.ox, self.oy
+        );
+        Ok(None)
+    }
+
+    pub fn deinit(&mut self) -> io::Result<()> {
+        self.term.disable_raw_mode()
     }
 
     pub fn open(&mut self, fname: &Path) -> io::Result<()> {
--- src/main.rs
+++ src/main.rs
@@ -11,125 +11,23 @@ mod keys;
 mod tty;
 mod utils;
 
-use editor::{Command, Editor};
-use keys::Direction::*;
-use keys::*;
-use tty::Terminal;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let t = Terminal::new(io::stdin(), io::stdout());
-    let mut e = Editor::new(t);
+    let t = tty::Terminal::new(io::stdin(), io::stdout());
+    let mut e = editor::Editor::new(t);
 
     if args.len() == 2 {
         let path = Path::new(&args[1]);
         e.open(path).expect("open");
     }
 
-    e.term.enable_raw_mode().expect("could not enable raw mode");
-    e.term.update_window_size().expect("update_window_size");
-    e.term.clear_screen().expect("clear_screen");
-    e.refresh_screen().expect("refresh_screen");
+    e.init().unwrap();
     loop {
-        e.term.update_window_size().expect("update_window_size");
-
-        let km = e.term.get_key().expect("get_key");
-        eprintln!("key: {:?}", km);
-        let KeyMod {
-            key,
-            ctrl,
-            meta,
-            shift,
-        } = km;
-        let cmd = match (ctrl, meta, shift, key) {
-            (true, false, false, Key::Char('q')) => Command::Exit,
-            (false, false, _, Key::Direction(d)) => Command::Move(d),
-            (true, false, _, Key::Direction(d)) => match d {
-                Direction::Up => Command::MovePageUp,
-                Direction::Down => Command::MovePageDown,
-                Direction::Left => Command::MoveLineHome,
-                Direction::Right => Command::MoveLineEnd,
-            },
-            (false, false, _, Key::Char(ch)) => Command::InsertCharacter(ch),
-            (false, false, _, Key::PageUp) => Command::MovePageUp,
-            (false, false, _, Key::PageDown) => Command::MovePageDown,
-            (false, false, _, Key::Home) => Command::MoveLineHome,
-            (false, false, _, Key::End) => Command::MoveLineEnd,
-            (false, false, _, Key::Backspace) => Command::Erase(Left),
-            (false, false, _, Key::Delete) => Command::Erase(Right),
-            (_, _, _, key) => {
-                eprintln!(
-                    "unhandled: {:?}",
-                    KeyMod {
-                        key,
-                        ctrl,
-                        meta,
-                        shift,
-                    }
-                );
-                Command::Nothing
-            }
-        };
-
-        match cmd {
-            Command::Nothing => continue,
-            Command::Exit => {
-                e.refresh_screen().expect("refresh_screen");
-                break;
-            }
-            Command::InsertCharacter(ch) => {
-                eprintln!("typing: {}", ch);
-            }
-            Command::Move(d) => match d {
-                Direction::Left => {
-                    if e.cx > 0 {
-                        e.cx -= 1
-                    }
-                }
-                Direction::Right => e.cx += 1,
-                Direction::Up => {
-                    if e.cy > 0 {
-                        e.cy -= 1
-                    }
-                }
-                Direction::Down => e.cy += 1,
-            },
-            Command::MovePageUp => {
-                e.cx = 0;
-                e.cy = 0;
-            }
-            Command::MovePageDown => {
-                e.cx = 0;
-                e.cy = e.term.wy - 1;
-            }
-            Command::MoveLineHome => {
-                e.cx = 0;
-            }
-            Command::MoveLineEnd => {
-                e.cx = e.term.wx - 1;
-            }
-            Command::Erase(Left) => {
-                if e.cx > 0 {
-                    e.cx -= 1
-                }
-            }
-            Command::Erase(Right) => e.cx += 1,
-            Command::Erase(_) => (),
+        match e.update().unwrap() {
+            Some(editor::Exit) => break,
+            None => continue,
         }
-        // Scroll to show cursor
-        e.ox = 0;
-        while (e.cx - e.ox) >= (e.term.wx as f32 * 0.90) as u32 {
-            e.ox += (e.term.wx as f32 * 0.85) as u32;
-        }
-        e.refresh_screen().expect("refresh_screen");
-        eprintln!(
-            "editor: [ cx: {}, cy: {}, ox: {}, oy: {} ]",
-            e.cx, e.cy, e.ox, e.oy
-        );
     }
-
-    e.term
-        .disable_raw_mode()
-        .expect("could not disable raw mode");
+    e.deinit().unwrap();
     eprintln!("e: {:?}", e);
 }
